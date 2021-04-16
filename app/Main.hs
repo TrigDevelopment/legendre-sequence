66ea8a5ca815dcc0dd2711b4604815c3d2692b7e
ArtemkaKokorin <xx.artem-kokorin@mail.ru>
2021-04-16 10:29:41 +0300

    Add basic jacobi symbol function

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Lib (jacobi)
 
 main :: IO ()
-main = someFunc
+main = putStrLn (show (jacobi 2 11))
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
 module Lib
-    ( someFunc
+    ( jacobi
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List (replicate, transpose)
+import Data.Bool (bool)
+
+jacobi :: Int -> Int -> Int
+jacobi = go
+  where
+    go 0 1 = 1
+    go 0 _ = 0
+    go x y
+      | even r = plusMinus (rem y 8 `elem` [3, 5]) (go (div r 2) y)
+      | otherwise = plusMinus (p r && p y) (go y r)
+      where
+        plusMinus = bool id negate
+        p = (3 ==) . flip rem 4
+        r = rem x y
