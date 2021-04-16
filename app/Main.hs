ea03afd1d8d99148605946e47035d13180f7c3b4
ArtemkaKokorin <xx.artem-kokorin@mail.ru>
2021-04-16 12:03:50 +0300

    Make reports for quarters

--- app/Main.hs
+++ app/Main.hs
@@ -22,20 +22,28 @@ jacobiRangesOnQuarters ::
   Integer -> -- Простое число p
   [Integer]
 jacobiRangesOnQuarters range_length prime =
-  [ jacobiRangeSum start range_length prime | start <- 
-    [ 1
-    , prime - toInteger range_length - 1
-    , div prime 4
-    , div prime 4 - toInteger range_length
-    , div prime 2
-    , div prime 2 - toInteger range_length
-    , div prime 4 * 3
-    , div prime 4 * 3 - toInteger range_length
-    ]
+  [ jacobiRangeSum start range_length prime 
+  | start <- quarterRangeStarts range_length prime 
+  ]
+
+quarterRangeStarts ::
+  Int -> -- Длина отрезка
+  Integer -> -- Простое число p
+  [Integer]
+quarterRangeStarts range_length prime =
+  [ 1
+  , prime - toInteger range_length
+  , div prime 4
+  , div prime 4 - toInteger range_length
+  , div prime 2
+  , div prime 2 - toInteger range_length
+  , div prime 4 * 3
+  , div prime 4 * 3 - toInteger range_length
   ]
 
 hints :: [String]
-hints = [ "1 (вправо): "
+hints = 
+  [ "1 (вправо): "
   , "1 (влево): "
   , "p/4 (вправо): "
   , "p/4 (влево): "
@@ -45,9 +53,26 @@ hints = [ "1 (вправо): "
   , "3p/4 (влево): "
   ]
 
+rangeReport ::
+  Integer -> -- Начало отрезка
+  Int -> -- Длина отрезка
+  Integer -> -- Простое число p
+  String
+rangeReport range_start range_length prime =
+  "Сумма на отрезке [" ++ 
+    show range_start ++ "; " ++ show (range_start + toInteger range_length - 1) ++
+    "]: " ++ show (jacobiRangeSum range_start range_length prime)
+
+report ::
+  Int -> -- Длина отрезка
+  Integer -> -- Простое число p
+  String
+report range_length prime =
+  "Длина отрезка: " ++ show range_length ++ ".\n" ++
+  "Простое число p: " ++ show prime ++ ".\n" ++ 
+  intercalate "\n" [rangeReport range_start range_length prime
+                   | range_start <- quarterRangeStarts range_length prime
+                   ]
+
 main :: IO ()
-main = putStrLn $ 
-  intercalate "\n" (map (\(a, b) -> a ++ b) $ zip
-    hints
-    (map show (jacobiRangesOnQuarters 2 bigPrime_10_12))
-    )
+main = putStrLn $ report 2 bigPrime_10_12
